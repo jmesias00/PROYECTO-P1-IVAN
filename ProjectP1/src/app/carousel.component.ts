448994980ec211a8c4594603f5896ace08598328
Jhosue Mesias <jhosuemesias@gmail.com>
2024-06-17 08:27:02 -0500

    Initial commit

---
+++ ProjectP1/src/app/carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+Component ({
+  selector: "app-carousel",
+  template:"./carousel.component.html",
+  styleUrls: ["./carousel.component.css"]
+})
+
+export class carouselComponent {
+
+}
